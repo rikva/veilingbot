f2f0ca25ec6936ea9f355241ad211aff0d130453
rik <rvachterberg@gmail.com>
2013-11-09 15:23:10 +0100

    Yay - log to file per auction

--- botmanager.py
+++ botmanager.py
@@ -9,23 +9,30 @@ parser.read('config.cfg')
 
 processes = []
 
+
+def _slugify(url):
+    for char in ['https://', 'http://', 'www.', '/', ':']:
+        url = url.replace(char, "")
+    return url
+
 try:
-    with open("veilingbot.log", "a") as logfile:
+    for section in parser.sections():
+        url = parser.get(section, 'url')
+        maxprice = parser.get(section, 'maxprice')
+        action = parser.get(section, 'action')
+        browser = parser.get(section, 'browser')
 
-        for section in parser.sections():
-            url = parser.get(section, 'url')
-            maxprice = parser.get(section, 'maxprice')
-            action = parser.get(section, 'action')
-            browser = parser.get(section, 'browser')
+        command = "python -u veilingbot.py %s %s %s %s" % (url, maxprice, browser, action)
 
-            command = "python -u veilingbot.py %s %s %s %s" % (url, maxprice, browser, action)
-            print "Starting process: %s" % command
-            processes.append(subprocess.Popen(command, stdout=logfile, stderr=logfile, shell=True))
+        logfile = _slugify(url) + ".log"
+        with open(logfile, "a") as log:
+            print "Starting process: %s - logging to file %s" % (command, logfile)
+            processes.append(subprocess.Popen(command, stdout=log, stderr=log, shell=True))
 
-            print "Sleeping 10 seconds."
-            sleep(10)
+        print "Sleeping 10 seconds."
+        sleep(10)
 
-        print "Done"
+    print "Done"
 
     # Start loop and check on processes
     while True:
@@ -45,3 +52,4 @@ except Exception:
     for p in processes:
         p.kill()
     raise
+
