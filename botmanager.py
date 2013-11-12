4a2e921a99409c6c343d6b99679473133161b2a4
rik <rvachterberg@gmail.com>
2013-11-12 21:58:48 +0100

    Trying some stuff like signals in botmanager

--- botmanager.py
+++ botmanager.py
@@ -3,11 +3,9 @@ import subprocess
 from ConfigParser import SafeConfigParser
 from time import sleep
 import sys
+import signal
 
-parser = SafeConfigParser()
-parser.read('config.cfg')
-
-processes = []
+PROCESSES = []
 
 
 def _slugify(url):
@@ -15,41 +13,66 @@ def _slugify(url):
         url = url.replace(char, "")
     return url
 
-try:
-    for section in parser.sections():
-        url = parser.get(section, 'url')
-        maxprice = parser.get(section, 'maxprice')
-        action = parser.get(section, 'action')
-        browser = parser.get(section, 'browser')
+def kill_processes():
+    global PROCESSES
 
-        command = "python -u veilingbot.py %s %s %s %s" % (url, maxprice, browser, action)
-
-        logfile = _slugify(url) + ".log"
-        with open(logfile, "a") as log:
-            print "Starting process: %s - logging to file %s" % (command, logfile)
-            processes.append(subprocess.Popen(command, stdout=log, stderr=log, shell=True))
-
-        print "Sleeping 10 seconds."
-        sleep(10)
-
-    print "Done"
-
-    # Start loop and check on processes
-    while True:
-        sleep(1)
-        if not processes:
-            print "No processes left, exiting"
-            sys.exit(0)
-
-        for process in processes:
-            if process.poll():
-                # process has died
-                print "One process has died with returncode %s." % process.returncode
-                processes.remove(process)
-
-except Exception:
-    print "Exception! Killing all processes..."
-    for p in processes:
+    print "Killing all subprocesses"
+    for p in PROCESSES:
         p.kill()
-    raise
+    PROCESSES = []
 
+def signal_handler_sigint(signal, frame):
+    print "Caught CTRL+C"
+    kill_processes()
+    sys.exit(0)
+
+def signal_handler_sighup(signal, frame):
+    print "Reloading..."
+    kill_processes()
+    main()
+
+def main():
+    try:
+        parser = SafeConfigParser()
+        parser.read('config.cfg')
+        for section in parser.sections():
+            url = parser.get(section, 'url')
+            maxprice = parser.get(section, 'maxprice')
+            action = parser.get(section, 'action')
+            browser = parser.get(section, 'browser')
+
+            command = "python -u veilingbot.py %s %s %s %s" % (url, maxprice, browser, action)
+
+            logfile = _slugify(url) + ".log"
+            with open(logfile, "a") as log:
+                print "Starting process: %s - logging to file %s" % (command, logfile)
+                PROCESSES.append(subprocess.Popen(command, stdout=log, stderr=log, shell=True))
+
+            print "Sleeping 10 seconds."
+            sleep(10)
+
+        print "Done"
+
+        # Start loop and check on processes & signals
+        while True:
+            sleep(1)
+            if not PROCESSES:
+                print "No processes left, exiting"
+                sys.exit(0)
+
+            for process in PROCESSES:
+                if process.poll():
+                    # process has died
+                    print "One process has died with returncode %s." % process.returncode
+                    PROCESSES.remove(process)
+
+            signal.signal(signal.SIGINT, signal_handler_sigint)
+            signal.signal(signal.SIGHUP, signal_handler_sighup)
+
+    except Exception:
+        print "Exception! Killing all processes..."
+        kill_processes()
+        raise
+
+if __name__ == "__main__":
+    main()
